18b816c94eac28a0f9d1179d382c098c76975f7e
Bolat Abdulaziz <Bola_cg56@student.itstep.kz>
2025-04-10 20:09:54 +0500

    feat: added CRUD requests to server

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,23 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 import { Product, ProductService } from '../../services/product.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrl: './products-list.component.css',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
 })
 export class ProductsListComponent implements OnInit {
   products: Product[] = [];
 
+  newProduct: Product = {
+    name: '',
+    description: '',
+    price: 0
+  };
+
   constructor(private productService: ProductService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getAllProducts();
+  }
+
+  getAllProducts() {
     this.productService.getAll().subscribe(data => {
-      console.log('Получены продукты: ', data);
       this.products = data;
+    });
+  }
+
+  getProductById(id: number) {
+    this.productService.getById(id).subscribe((product) => {
+      console.log('Получен продукт:', product);
+    })
+  }
+
+  createProduct() {
+    this.productService.create(this.newProduct).subscribe((created) => {
+      console.log('Создан продукт:', created);
+      this.getAllProducts();
+
+      this.newProduct = {
+        name: '',
+        description: '',
+        price: 0
+      };
+    });
+  }
+
+  updateProduct(product: Product) {
+    const updatedProduct = { ...product, price: product.price + 10 };
+
+    this.productService.update(product.id!, updatedProduct).subscribe((updated) => {
+      console.log('Обновлен продукт:', updated);
+      this.getAllProducts();
+    });
+  }
+
+  deleteProduct(id: number) {
+    this.productService.delete(id).subscribe(() => {
+      console.log('Удален продукт:', id);
+      this.getAllProducts();
     });
   }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -20,5 +20,20 @@ export class ProductService {
   getAll(): Observable<Product[]> {
     return this.http.get<Product[]>(this.apiUrl);
   }
-  
+ 
+  getById(id: number): Observable<Product> {
+    return this.http.get<Product>(`${this.apiUrl}/${id}`)
+  }
+
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.apiUrl, product);
+  }
+
+  update(id: number, product: Product): Observable<Product> {
+    return this.http.put<Product>(`${this.apiUrl}/${id}`, product);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`)
+  }
 }
